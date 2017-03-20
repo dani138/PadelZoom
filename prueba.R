7a59e373752d4d088a40186989f9dd01090de824
Dani <danielsanchez13.8@gmail.com>
2017-03-20 12:31:02 +0100

    actualizar prueba.R

--- prueba.R
+++ prueba.R
@@ -1,6 +1,12 @@
-library(googleAnalyticsR)
-library(plyr)
-library(ggplot2)
+# List of packages for session
+.packages = c("googleAnalyticsR","plyr","ggplot2")
+
+# Install CRAN packages (if not already installed)
+.inst <- .packages %in% installed.packages()
+if(length(.packages[!.inst]) > 0) install.packages(.packages[!.inst])
+
+# Load packages into session 
+lapply(.packages, require, character.only=TRUE)
 
 ga_auth()
 
